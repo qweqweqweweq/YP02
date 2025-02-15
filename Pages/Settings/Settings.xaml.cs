70904966cf5e372b26a6765055fc9bd68c747091
Milosh <mutagarova05@inbox.ru>
2025-02-15 12:15:55 +0500

    rbkjvtnh

--- Pages/Settings/Settings.xaml.cs
+++ Pages/Settings/Settings.xaml.cs
@@ -24,5 +24,25 @@ namespace YP02.Pages.Settings
         {
             InitializeComponent();
         }
+
+        private void KeyDown_Search(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void SortUp(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SortDown(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(new Pages.Main());
+        }
     }
 }
