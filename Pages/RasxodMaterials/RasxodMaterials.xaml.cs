9073277cf545bb248bb8d5e72376fa949a3e3466
Dasha <rotanovad@bk.ru>
2025-03-23 03:21:56 +0500

    Реализован экспорт документа расходных материалов. Ротанова

--- Pages/RasxodMaterials/RasxodMaterials.xaml.cs
+++ Pages/RasxodMaterials/RasxodMaterials.xaml.cs
@@ -92,93 +92,110 @@ namespace YP02.Pages.RasxodMaterials
 
         private void ExportRasxodMaterials(object sender, RoutedEventArgs e)
         {
-            //// Получаем текущую дату
-            //string currentDate = DateTime.Now.ToString("dd.MM.yyyy");
+            // Получаем текущую дату
+            string currentDate = DateTime.Now.ToString("dd.MM.yyyy");
 
-            //// Создаем экземпляр контекста
-            //using (var obContext = new OborudovanieContext())
-            //{
-            //    // Получаем ID оборудования
-            //    int selectedEquipmentId = GetSelectedEquipmentId(); // Метод для получения ID выбранного оборудования
+            // Создаем экземпляр контекста
+            using (var rasxMatContext = new RasxodMaterialsContext())
+            {
+                // Получаем ID расходника
+                int selectedEquipmentId = GetSelectedEquipmentId();
 
-            //    // Получаем данные об оборудовании из базы данных по ID
-            //    var oborudovanie = obContext.Oborudovanie
-            //        .FirstOrDefault(x => x.Id == selectedEquipmentId);
+                // Получаем данные об расходных материалах из базы данных по ID
+                var rasMat = rasxMatContext.RasxodMaterials
+                    .FirstOrDefault(x => x.Id == selectedEquipmentId);
 
-            //    if (oborudovanie == null)
-            //    {
-            //        MessageBox.Show("Оборудование не найдено в базе данных.");
-            //        return;
-            //    }
+                if (rasMat == null)
+                {
+                    MessageBox.Show("Расходные материалы не найдены в базе данных.");
+                    return;
+                }
 
-            //    // Получаем текущего пользователя
-            //    var currentUser = usContext.Users.FirstOrDefault(x => x.Role == "Сотрудник");
+                // Получаем текущего пользователя
+                var currentUser = usContext.Users.FirstOrDefault(x => x.Role == "Сотрудник");
 
-            //    // Создаем новый документ
-            //    using (DocX document = DocX.Create("Akt_Priema_Peredachi.docx"))
-            //    {
-            //        // Добавляем заголовок
-            //        document.InsertParagraph("АКТ\nприема-передачи расходных материалов\n\n")
-            //            .Font("Times New Roman")
-            //            .FontSize(12)
-            //            .Alignment = Alignment.center;
+                // Создаем новый документ
+                using (DocX document = DocX.Create("Akt_Priema_Peredachi_Rasxodnyx_Materialov.docx"))
+                {
+                    // Добавляем заголовок
+                    document.InsertParagraph("АКТ\nприема-передачи расходных материалов\n\n")
+                        .Font("Times New Roman")
+                        .FontSize(12)
+                        .Alignment = Alignment.center;
 
-            //        // Добавляем информацию о месте и дате
-            //        var locationAndDate = document.InsertParagraph($"г. Пермь")
-            //            .Font("Times New Roman")
-            //            .FontSize(12)
-            //            .Alignment = Alignment.left;
+                    // Добавляем информацию о месте и дате
+                    var locationAndDate = document.InsertParagraph($"г. Пермь")
+                        .Font("Times New Roman")
+                        .FontSize(12)
+                        .Alignment = Alignment.left;
 
-            //        var date = document.InsertParagraph($"{currentDate}\n")
-            //            .Font("Times New Roman")
-            //            .FontSize(12)
-            //            .Alignment = Alignment.right;
+                    var date = document.InsertParagraph($"{currentDate}\n")
+                        .Font("Times New Roman")
+                        .FontSize(12)
+                        .Alignment = Alignment.right;
 
-            //        if (currentUser != null)
-            //        {
-            //            var fioParts = currentUser.FIO.Split(' ');
-            //            string lastName = fioParts[0]; // Фамилия
-            //            string initials = $"{fioParts[1][0]}.{fioParts[2][0]}."; // Инициалы (Имя и Отчество)
-            //            // Добавляем основной текст с отступом
-            //            var mainText = document.InsertParagraph($"КГАПОУ Пермский Авиационный техникум им. А.Д. Швецова в целях\nобеспечения необходимым оборудованием для исполнения должностных обязанностей\nпередаёт сотруднику {lastName} {initials}, а сотрудник принимает от учебного учреждения\nследующее оборудование:\n\n");
-            //            mainText.Font("Times New Roman");
-            //            mainText.FontSize(12);
-            //            mainText.IndentationFirstLine = 26;
-            //            mainText.Alignment = Alignment.both;
-            //        }
+                    if (currentUser != null)
+                    {
+                        var fioParts = currentUser.FIO.Split(' ');
+                        string lastName = fioParts[0]; // Фамилия
+                        string initials = $"{fioParts[1][0]}.{fioParts[2][0]}."; // Инициалы (Имя и Отчество)
+                        // Добавляем основной текст с отступом
+                        var mainText = document.InsertParagraph($"КГАПОУ Пермский Авиационный техникум им. А.Д. Швецова в целях\nобеспечения необходимым оборудованием для исполнения должностных обязанностей\nпередаёт сотруднику {lastName} {initials}, а сотрудник принимает от учебного учреждения\nследующие расходные материалы:\n\n");
+                        mainText.Font("Times New Roman");
+                        mainText.FontSize(12);
+                        mainText.IndentationFirstLine = 26;
+                        mainText.Alignment = Alignment.both;
+                    }
 
-            //        using (var viewContext = new ViewModelContext())
-            //        {
-            //            int selectEquipmentId = GetSelectEquipmentId();
-            //            // Получаем модель оборудования по IdModelObor
-            //            var model = viewContext.ViewModel
-            //                .FirstOrDefault(x => x.Id == selectEquipmentId);
+                    using (var typeContext = new TypeCharacteristicsContext())
+                    {
+                        int selectEquipmentId = GetSelectEquipmentId();
+                        // Получаем тип расходника
+                        var typeChar = typeContext.TypeCharacteristics
+                            .FirstOrDefault(x => x.Id == selectEquipmentId);
 
+                        using (var valueContext = new ValueCharacteristicsContext())
+                        {
+                            int selctEquipmentId = GetSelectEquipmentId();
+                            var valueChar = valueContext.ValueCharacteristics
+                            .FirstOrDefault(x => x.Id == selctEquipmentId);
 
-            //            // Добавляем информацию об оборудовании в одной строке и по центру
-            //            var equipmentInfo = document.InsertParagraph($" {oborudovanie.Name} {model.Name}, серийный номер {oborudovanie.InventNumber}, стоимостью {oborudovanie.PriceObor} руб. \n\n\n")
-            //                .Font("Times New Roman")
-            //                .FontSize(12)
-            //                .Alignment = Alignment.center;
-            //        }
-            //        // Извлекаем фамилию и инициалы
-            //        if (currentUser != null)
-            //        {
-            //            var fioParts = currentUser.FIO.Split(' ');
-            //            string lastName = fioParts[0]; // Фамилия
-            //            string initials = $"{fioParts[1][0]}.{fioParts[2][0]}."; // Инициалы (Имя и Отчество)
-            //            var paragraph = document.InsertParagraph($"{lastName} {initials}       ____________________     ________________")
-            //                .Font("Times New Roman")
-            //                .FontSize(12)
-            //                .Alignment = Alignment.left;
-            //        }
+                            // Добавляем информацию об расходных материалах в одной строке и по центру
+                            var equipmentInfo = document.InsertParagraph($" {typeChar.Name} {rasMat.Name}, в количестве {rasMat.Quantity} шт, стоимостью {valueChar.Znachenie} руб. \n\n\n")
+                                .Font("Times New Roman")
+                                .FontSize(12)
+                                .Alignment = Alignment.center;
+                        }
+                        
+                    }
+                    // Извлекаем фамилию и инициалы
+                    if (currentUser != null)
+                    {
+                        var fioParts = currentUser.FIO.Split(' ');
+                        string lastName = fioParts[0]; // Фамилия
+                        string initials = $"{fioParts[1][0]}.{fioParts[2][0]}."; // Инициалы (Имя и Отчество)
+                        var paragraph = document.InsertParagraph($"{lastName} {initials}       ____________________     ________________")
+                            .Font("Times New Roman")
+                            .FontSize(12)
+                            .Alignment = Alignment.left;
+                    }
 
-            //        // Сохраняем документ
-            //        document.Save();
-            //    }
+                    // Сохраняем документ
+                    document.Save();
+                }
 
-            //    MessageBox.Show("Документ успешно сгенерирован по пути: Desktop\\YP02\\bin\\Debug\\net6.0-windows");
-            //}
+                MessageBox.Show("Документ успешно сгенерирован по пути: Desktop\\YP02\\bin\\Debug\\net6.0-windows");
+            }
+        }
+
+        private int GetSelectedEquipmentId()
+        {
+            return 1;
+        }
+
+        private int GetSelectEquipmentId()
+        {
+            return 1;
         }
     }
 }
